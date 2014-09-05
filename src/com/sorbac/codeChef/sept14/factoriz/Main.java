168451f8a467dff9cc2dfc5e6297fd2b06ae8d06
Stanislav Kovalcin <stanislav.kovalcin@deutsche-boerse.com>
2014-09-05 15:34:10 +0200

    NZEC solution for Cleaning Tables problem - http://www.codechef.com/AUG14/problems/CLETAB

---
+++ src/com/sorbac/codeChef/sept14/factoriz/Main.java
@@ -0,0 +1,19 @@
+package com.sorbac.codeChef.sept14.factoriz;
+
+/*
+ *  Copyright 1999,2001 Clearstream Services, Luxembourg.
+ *  All rights reserved.
+ *
+ *  This software is the confidential and proprietary information
+ *  of Clearstream Services ("Confidential Information").  You
+ *  shall not disclose such Confidential Information and shall use
+ *  it only in accordance with the terms of the license agreement
+ *  you entered into with CS.
+ *
+ *  @author: Stanislav Kovalcin (ky234) |HUB team|
+ *  Date: 05/09/14
+ *  Time: 11:47   
+ *
+ */
+public class Main {
+}
